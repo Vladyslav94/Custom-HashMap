d00fcf731d84413779dc4847ce18cee015f6eb92
Vladyslav94 <grigoren_vlad2009@yahoo.com>
2020-07-13 15:06:51 +0300

    refactored HashMapImplementation

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -9,39 +9,39 @@ public class Tester {
     @Test
     public void testAssertMap() {
 
-        HashMapImplementation<Integer, Long> hashMapRealization = new HashMapImplementation<>();
+        HashMapImplementation<Integer, Long> hashMapImplementation = new HashMapImplementation<>();
 
-        hashMapRealization.put(3, 15L);
-        hashMapRealization.put(-6, 105L);
-        hashMapRealization.put(32312, -70L);
-        hashMapRealization.put(-455, 1050L);
-        hashMapRealization.put(11, 88L);
-        hashMapRealization.put(-42352352, 213123123211321312L);
-        hashMapRealization.put(-6, 105L);
-        hashMapRealization.put(5, 0);
-        hashMapRealization.put(0, 0);
+        hashMapImplementation.put(3, 15L);
+        hashMapImplementation.put(-6, 105L);
+        hashMapImplementation.put(32312, -70L);
+        hashMapImplementation.put(-455, 1050L);
+        hashMapImplementation.put(11, 88L);
+        hashMapImplementation.put(-42352352, 213123123211321312L);
+        hashMapImplementation.put(-6, 105L);
+        hashMapImplementation.put(5, 0);
+        hashMapImplementation.put(0, 0);
 
 
         //Test size
-        assertThat(hashMapRealization.size(), is(7));
+        assertThat(hashMapImplementation.size(), is(7));
 
         //Test get (values)
-        assertEquals(hashMapRealization.get(3), 15L);
-        assertEquals(hashMapRealization.get(32312), -70L);
-        assertEquals(hashMapRealization.get(-455), 1050L);
-        assertEquals(hashMapRealization.get(11), 88L);
-        assertEquals(hashMapRealization.get(-42352352), 213123123211321312L);
-        assertEquals(hashMapRealization.get(-6), 105L);
-        assertEquals(hashMapRealization.get(5), 0);
-        assertEquals(hashMapRealization.get(54234), 0);
+        assertEquals(hashMapImplementation.get(3), 15L);
+        assertEquals(hashMapImplementation.get(32312), -70L);
+        assertEquals(hashMapImplementation.get(-455), 1050L);
+        assertEquals(hashMapImplementation.get(11), 88L);
+        assertEquals(hashMapImplementation.get(-42352352), 213123123211321312L);
+        assertEquals(hashMapImplementation.get(-6), 105L);
+        assertEquals(hashMapImplementation.get(5), 0);
+        assertEquals(hashMapImplementation.get(54234), 0);
 
         //Test put (keys)
-        assertTrue(hashMapRealization.contains(3));
-        assertTrue(hashMapRealization.contains(32312));
-        assertTrue(hashMapRealization.contains(-455));
-        assertTrue(hashMapRealization.contains(11));
-        assertTrue(hashMapRealization.contains(-42352352));
-        assertTrue(hashMapRealization.contains(-6));
+        assertTrue(hashMapImplementation.contains(3));
+        assertTrue(hashMapImplementation.contains(32312));
+        assertTrue(hashMapImplementation.contains(-455));
+        assertTrue(hashMapImplementation.contains(11));
+        assertTrue(hashMapImplementation.contains(-42352352));
+        assertTrue(hashMapImplementation.contains(-6));
 
     }
 
